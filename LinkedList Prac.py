0ed48210e80d95649bbaa26e8c6783dc52849946
irtiza1999 <mdirtiza.hossain1999@gmail.com>
2021-01-13 01:20:02 +0600

    DSLL done

--- LinkedList Prac.py
+++ LinkedList Prac.py
@@ -241,3 +241,140 @@
 # print(list(node.value for node in circularSLL))
 # circularSLL.deleteAll()
 # print(list(node.value for node in circularSLL))
+
+
+# Doubly Linked List
+
+class Node:
+    def __init__(self, value=None):
+        self.prev = None
+        self.value = value
+        self.next = None
+
+
+class DoublySLL:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
+    def createDSLL(self, nodeValue):
+        node = Node(nodeValue)
+        node.prev = None
+        node.next = None
+        self.head = node
+        self.tail = node
+        print("DSLL created!!")
+
+    def insertNode(self, nodeValue, location):
+        if self.head is None:
+            print("The node can not be inserted!!")
+        else:
+            newNode = Node(nodeValue)
+            if location == 0:
+                newNode.prev = None
+                newNode.next = self.head
+                self.head.prev = newNode
+                self.head = newNode
+            elif location == 1:
+                newNode.next = None
+                newNode.prev = self.tail
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                newNode.next = tempNode.next
+                newNode.prev = tempNode
+                newNode.next.prev = newNode
+                tempNode.next = newNode
+
+    def treversalDSLL(self):
+        if self.head is None:
+            print("No DSLL!!")
+        else:
+            tempNode = self.head
+            while tempNode:
+                print(tempNode.value, end=" ")
+                tempNode = tempNode.next
+
+    def reverseDSLL(self):
+        if self.head is None:
+            print("No emelmets!!")
+        else:
+            tempNode = self.tail
+            while tempNode:
+                print(tempNode.value, end=" ")
+                tempNode = tempNode.prev
+
+    def searchNode(self, nodeValue):
+        if self.head is None:
+            print("No elements!!")
+        else:
+            tempNode = self.head
+            while tempNode:
+                if tempNode.value == nodeValue:
+                    return tempNode.value
+
+                tempNode = tempNode.next
+            return "Node not in list!!"
+
+    def deleteNode(self, location):
+        if self.head is None:
+            print("No elemet in list!!")
+        else:
+            if location == 0:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+                    self.head.prev = None
+            elif location == 1:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.tail = self.tail.prev
+                    self.tail.next = None
+            else:
+                curNode = self.head
+                index = 0
+                while index < location - 1:
+                    curNode = curNode.next
+                    index += 1
+                curNode.next = curNode.next.next
+                curNode.next.prev = curNode
+                print("Success..")
+
+    def deleteAll(self):
+        if self.head is None:
+                print("No head is here!!")
+        else:
+            tempNode = self.head
+            while tempNode:
+                tempNode.prev = None
+                tempNode = tempNode.next
+            self.head = None
+            self.tail = None
+            print("Deleted!!")
+
+doublySLL = DoublySLL()
+doublySLL.createDSLL(1)
+print(list(node.value for node in doublySLL))
+doublySLL.insertNode(2, 0)
+doublySLL.insertNode(3, 1)
+doublySLL.insertNode(4, 1)
+print(list(node.value for node in doublySLL))
+print(doublySLL.deleteNode(-1))
+print(list(node.value for node in doublySLL))
+doublySLL.deleteAll()
+print(list(node.value for node in doublySLL))
