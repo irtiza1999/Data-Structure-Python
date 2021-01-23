58e5181ad4be5c9968d91432a45c4c149d8ab11d
irtiza1999 <mdirtiza.hossain1999@gmail.com>
2021-01-23 18:05:41 +0600

    finish

--- houseRobber.py
+++ houseRobber.py
@@ -1,5 +1,6 @@
 # Given N number of houses along the street with some ammount of money. Adjacent houses cannot be stolen. Find the maximum ammount that can be stolen.
 
+# Divide and conqure
 def houseRobber(houses, currentIndex):
     if currentIndex >= len(houses):
         return 0
@@ -9,5 +10,23 @@ def houseRobber(houses, currentIndex):
         return max(steal1, steal2)
 
 
+# Top Down]
+def houseRobberTD(houses, currentIndex, tempDict):
+    if currentIndex >= len(houses):
+        return 0
+    else:
+        if currentIndex not in tempDict:
+            stealFirst = houses[currentIndex] + houseRobberTD(houses, currentIndex + 2, tempDict)
+            skipFirst = houseRobberTD(houses, currentIndex + 1, tempDict)
+            tempDict[currentIndex] = max(stealFirst, skipFirst)
+        return tempDict[currentIndex]
+
+
+def houseRobberBU(houses, currentIndex):
+    tempArr = [0] * (len(houses) + 2)
+    for i in range(len(houses) - 1, -1, -1):
+        tempArr[i] = max(houses[i] + tempArr[i+2], tempArr[i+1])
+    return tempArr[0]
+
 houses = [6, 7, 1, 30, 8, 2, 4]
-print(houseRobber(houses, 0))
+print(houseRobberBU(houses, 0))
--- numberFactor.py
+++ numberFactor.py
@@ -1,5 +1,6 @@
 # Given N, find the number of ways to express N as a sum of 1, 3, 4.
 
+# Divide and conqure
 def numberFactor(n):
     if n in (0, 1, 2):
         return 1
@@ -12,4 +13,29 @@ def numberFactor(n):
         return sub1 + sub2 + sub3
 
 
-print(numberFactor(15))
+# Top Down Approach
+
+def numberFactorTD(n, tempdict):
+    if n in (0, 1, 2):
+        return 1
+    elif n == 3:
+        return 2
+    else:
+        if n not in tempdict:
+            sub1 = numberFactorTD(n - 1, tempdict)
+            sub2 = numberFactorTD(n - 3, tempdict)
+            sub3 = numberFactorTD(n - 4, tempdict)
+            tempdict[n] = sub1 + sub2 + sub3
+        return tempdict[n]
+
+
+# Bottom up approach
+
+def numberFactorBU(n):
+    tempArr = [1, 1, 1, 2]
+    for i in range(4, n + 1):
+        tempArr.append(tempArr[i - 1] + tempArr[i - 3] + tempArr[i - 4])
+    return tempArr[n]
+
+
+print(numberFactorBU(5))
